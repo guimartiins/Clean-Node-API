420a71bc240b2fbb25c99e81ad8e665e08716c70
castmoon <academicgmag@gmail.com>
2021-08-12 14:17:57 -0300

    feat: ensure SignUpController return an error if no password is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ import { IRequest, IResponse } from '../protocols/http';
 
 export class SignUpController {
     handle(httpRequest: IRequest): IResponse {
-        const requiredFields = ['name', 'email'];
+        const requiredFields = ['name', 'email', 'password'];
         for (const field of requiredFields) {
             if (!httpRequest.body[field]) {
                 return badRequest(new MissingParamError(field));
