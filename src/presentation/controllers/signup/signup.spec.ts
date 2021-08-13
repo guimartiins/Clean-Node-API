9fe549054b0aea6194bd4ddae76c2abb35bc5fab
castmoon <academicgmag@gmail.com>
2021-08-13 14:22:19 -0300

    test: ensure SignUpController returns 500 if AddAccount throws

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -193,4 +193,22 @@ describe('SignUp Controller', () => {
             password: 'password',
         });
     });
+
+    test('should return 500 if an AddAccount throws', () => {
+        const { sut, addAccountStub } = makeSut();
+        jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+            throw new Error();
+        });
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@email.com',
+                password: 'password',
+                passwordConfirmation: 'password',
+            },
+        };
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(500);
+        expect(httpResponse.body).toEqual(new ServerError());
+    });
 });
