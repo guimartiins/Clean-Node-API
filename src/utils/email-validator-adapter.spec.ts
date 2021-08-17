7755b7c9262f6810ae3ab59be0c0f353bf5784e8
castmoon <academicgmag@gmail.com>
2021-08-17 14:21:46 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -20,4 +20,10 @@ describe('EmailValidator Adapter', () => {
         const isValid = sut.isValid('valid_email@gmail.com');
         expect(isValid).toBe(true);
     });
+    test('should call validator with correct email', () => {
+        const sut = new EmailValidatorAdapter();
+        const isEmailSpy = jest.spyOn(validator, 'isEmail');
+        sut.isValid('valid_email@email.com');
+        expect(isEmailSpy).toHaveBeenCalledWith('valid_email@email.com');
+    });
 });
