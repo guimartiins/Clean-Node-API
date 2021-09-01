4aaf714a366fc35ea64aefe293250ba60fee7cd2
castmoon <academicgmag@gmail.com>
2021-09-01 11:39:05 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest';
+
+import app from '../config/app';
+
+describe('Body Parser Middleware', () => {
+    test('should parse body as json', async () => {
+        app.post('/test-body-parser', (req, res) => res.send(req.body));
+        await request(app)
+            .post('/test-body-parser')
+            .send({ name: 'gui' })
+            .expect({ name: 'gui' });
+    });
+});
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,3 @@
-import express from 'express';
-
-const app = express();
+import app from './config/app';
 
 app.listen(3000, () => console.log('Server started at port 3000'));
