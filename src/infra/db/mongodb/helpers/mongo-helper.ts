66422dc79e1ad207f8c94d5f8689a9ea54d59083
castmoon <academicgmag@gmail.com>
2021-08-31 21:51:47 -0300

    refactor: add return type to disconnect method on mongo helper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,13 +3,14 @@ import { MongoClient, Collection } from 'mongodb';
 export const MongoHelper = {
     client: null as unknown as MongoClient,
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async connect(uri: string): Promise<void> {
         this.client = await MongoClient.connect(
             process.env.MONGO_URL as string
         );
     },
 
-    async disconnect() {
+    async disconnect(): Promise<void> {
         await this.client.close();
     },
 
