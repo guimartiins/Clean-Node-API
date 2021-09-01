e56a57998ef9a36d9e8fa702db7e1d2eb4383649
castmoon <academicgmag@gmail.com>
2021-08-31 21:47:47 -0300

    feat: ensure AccountMongoRepository return an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,17 +1,19 @@
-import { MongoClient } from 'mongodb';
-import { disconnect } from 'process';
+import { MongoClient, Collection } from 'mongodb';
 
 export const MongoHelper = {
     client: null as unknown as MongoClient,
 
     async connect(uri: string): Promise<void> {
-        this.client = await MongoClient.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        this.client = await MongoClient.connect(
+            process.env.MONGO_URL as string
+        );
     },
 
     async disconnect() {
         await this.client.close();
+    },
+
+    getCollection(name: string): Collection {
+        return this.client.db().collection(name);
     },
 };
