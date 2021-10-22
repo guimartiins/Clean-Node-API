d277528826821e3e8a087a8621e00323c42101d3
castmoon <academicgmag@gmail.com>
2021-10-21 21:29:35 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { InvalidParamError } from '../../errors/invalid-param-error';
 import { MissingParamError } from '../../errors/missing-param-error';
-import { badRequest } from '../../helpers/http-helper';
+import { badRequest, serverError } from '../../helpers/http-helper';
 import { IRequest, IResponse } from '../../protocols';
 import { IController } from '../../protocols/controller';
 import { IEmailValidator } from '../../protocols/email-validator';
@@ -9,21 +9,25 @@ import { IEmailValidator } from '../../protocols/email-validator';
 export class LoginController implements IController {
     constructor(private emailValidator: IEmailValidator) { }
     async handle(httpRequest: IRequest): Promise<IResponse> {
-        const { email, password } = httpRequest.body;
-        if (!email) {
-            return badRequest(new MissingParamError('email'));
-        }
-        if (!password) {
-            return badRequest(new MissingParamError('password'));
-        }
-        const isValid = this.emailValidator.isValid(email);
+        try {
+            const { email, password } = httpRequest.body;
+            if (!email) {
+                return badRequest(new MissingParamError('email'));
+            }
+            if (!password) {
+                return badRequest(new MissingParamError('password'));
+            }
+            const isValid = this.emailValidator.isValid(email);
 
-        if (!isValid) {
-            return badRequest(new InvalidParamError('email'))
+            if (!isValid) {
+                return badRequest(new InvalidParamError('email'))
+            }
+            return {
+                statusCode: 200,
+                body: {},
+            };
+        } catch (error) {
+            return serverError(error)
         }
-        return {
-            statusCode: 200,
-            body: {},
-        };
     }
 }
