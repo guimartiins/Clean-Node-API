073c2cb951151cd914f2d728b3fd55b0ebb23509
castmoon <academicgmag@gmail.com>
2021-10-21 21:59:11 -0300

    test: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -3,6 +3,7 @@ import {
     badRequest,
     serverError,
     unauthorized,
+    ok,
 } from '../../helpers/http-helper';
 import { LoginController } from './login';
 import { IEmailValidator, IRequest, IAuthentication } from './login-protocols';
@@ -129,5 +130,15 @@ describe('Login Controller', () => {
         );
         const httpResponse = await sut.handle(makeFakeRequest());
         expect(httpResponse).toEqual(serverError(new Error()));
+    });
+
+    test('should returns 200 if valid credentials are provided', async () => {
+        const { sut } = makeSut();
+        const httpResponse = await sut.handle(makeFakeRequest());
+        expect(httpResponse).toEqual(
+            ok({
+                accessToken: 'any_token',
+            })
+        );
     });
 });
