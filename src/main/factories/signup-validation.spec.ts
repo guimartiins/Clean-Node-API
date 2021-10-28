3d051b95a420e56c8e63aad6a93746b4abe75f83
castmoon <academicgmag@gmail.com>
2021-10-28 11:16:56 -0300

    refactor: move password comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,3 +1,4 @@
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation';
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation';
 import { IValidation } from '../../presentation/helpers/validators/validation';
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite';
@@ -17,6 +18,9 @@ describe('SignUpValidation Factory', () => {
         ]) {
             validations.push(new RequiredFieldValidation(field));
         }
+        validations.push(
+            new CompareFieldsValidation('password', 'passwordConfirmation')
+        );
         expect(ValidationComposite).toHaveBeenCalledWith(validations);
     });
 });
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -28,24 +28,7 @@ export class SignUpController implements IController {
             if (error) {
                 return badRequest(error);
             }
-            const requiredFields = [
-                'name',
-                'email',
-                'password',
-                'passwordConfirmation',
-            ];
-            for (const field of requiredFields) {
-                if (!httpRequest.body[field]) {
-                    return badRequest(new MissingParamError(field));
-                }
-            }
-            const { name, email, password, passwordConfirmation } =
-                httpRequest.body;
-            if (password !== passwordConfirmation) {
-                return badRequest(
-                    new InvalidParamError('passwordConfirmation')
-                );
-            }
+            const { name, email, password } = httpRequest.body;
             const isValid = this.emailValidator.isValid(email);
 
             if (!isValid) {
