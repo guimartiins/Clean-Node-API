0867aee78ee9964733e9ceca05e9766712c9e3bb
castmoon <academicgmag@gmail.com>
2021-11-01 20:56:02 -0300

    refactor: move signup to a folder

---
+++ src/main/factories/signup/signup.ts
@@ -0,0 +1,24 @@
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account';
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter';
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account';
+import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log';
+import { SignUpController } from '../../../presentation/controllers/signup/signup';
+import { IController } from '../../../presentation/protocols/controller';
+import { LogControllerDecorator } from '../../decorators/log';
+import { makeSignUpValidation } from './signup-validation';
+
+export const makeSignUpController = (): IController => {
+    const salt = 12;
+    const bcryptAdapter = new BcryptAdapter(salt);
+    const accountMongoRepository = new AccountMongoRepository();
+    const logMongoRepository = new LogMongoRepository();
+    const dbAddAccount = new DbAddAccount(
+        bcryptAdapter,
+        accountMongoRepository
+    );
+    const signUpController = new SignUpController(
+        dbAddAccount,
+        makeSignUpValidation()
+    );
+    return new LogControllerDecorator(signUpController, logMongoRepository);
+};
