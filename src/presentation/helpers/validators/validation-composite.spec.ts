d0e229cccdce747c3e9043356df541b060c71b74
castmoon <academicgmag@gmail.com>
2021-10-31 22:01:28 -0300

    test: ensure ValidationComposite returns the first error if more then one validation fails

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -4,7 +4,7 @@ import { ValidationComposite } from './validation-composite';
 
 interface ISutTypes {
     sut: ValidationComposite;
-    validationStub: IValidation;
+    validationStubs: IValidation[];
 }
 const makeValidation = (): IValidation => {
     class ValidationStub implements IValidation {
@@ -17,21 +17,33 @@ const makeValidation = (): IValidation => {
 };
 
 const makeSut = (): ISutTypes => {
-    const validationStub = makeValidation();
-    const sut = new ValidationComposite([validationStub]);
+    const validationStubs = [makeValidation(), makeValidation()];
+    const sut = new ValidationComposite(validationStubs);
     return {
         sut,
-        validationStub,
+        validationStubs,
     };
 };
 
 describe('Validation Composite', () => {
     test('should return an error if any validation fails', () => {
-        const { sut, validationStub } = makeSut();
-        jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+        const { sut, validationStubs } = makeSut();
+        jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(
             new MissingParamError('field')
         );
         const error = sut.validate({ field: 'any_value' });
         expect(error).toEqual(new MissingParamError('field'));
+    });
+
+    test('should return the first error if more then one validation fails', () => {
+        const { sut, validationStubs } = makeSut();
+        jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(
+            new Error()
+        );
+        jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(
+            new MissingParamError('field')
+        );
+        const error = sut.validate({ field: 'any_value' });
+        expect(error).toEqual(new Error());
     });
 });
