7dde6bc0e25a49af2758e3a7daf23fd380f9a231
castmoon <academicgmag@gmail.com>
2021-11-02 13:35:14 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import {
+    IAuthentication,
+    IAuthenticationModel,
+} from '../../../domain/usecases/authentication';
+import { ILoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository';
+
+export class DbAuthentication implements IAuthentication {
+    constructor(
+        private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository
+    ) { }
+    async auth(authentication: IAuthenticationModel): Promise<string | null> {
+        await this.loadAccountByEmailRepository.load(authentication.email);
+        return null;
+    }
+}
