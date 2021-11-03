9a2a9d0fe4cd614770eca4e106c68bdabdb9b335
castmoon <academicgmag@gmail.com>
2021-11-03 19:46:45 -0300

    test: ensure DbAuthentication returns null if HashComparer returns false

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -103,4 +103,13 @@ describe('DbAuthentication UseCase', () => {
         const promise = sut.auth(makeFakeAuthentication());
         expect(promise).rejects.toThrow();
     });
+
+    test('should return null if HashComparer returns false', async () => {
+        const { sut, hashComparerStub } = makeSut();
+        jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(
+            new Promise((resolve) => resolve(false))
+        );
+        const accessToken = await sut.auth(makeFakeAuthentication());
+        expect(accessToken).toBeNull();
+    });
 });
