6a4874ac5d4a201b195968386d07245311af2a00
castmoon <academicgmag@gmail.com>
2021-11-03 19:43:30 -0300

    feat: ensure DbAuthentication calls HashComparer witch correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -3,15 +3,20 @@ import {
     IAuthentication,
     IAuthenticationModel,
 } from '../../../domain/usecases/authentication';
+import { IHashComparer } from '../../protocols/criptography/hash-comparer';
 import { ILoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
 
 export class DbAuthentication implements IAuthentication {
     constructor(
-        private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository
+        private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository,
+        private readonly hashComparer: IHashComparer
     ) { }
 
     async auth(authentication: IAuthenticationModel): Promise<string | null> {
-        await this.loadAccountByEmailRepository.load(authentication.email);
+        const account = await this.loadAccountByEmailRepository.load(authentication.email);
+        if (account) {
+            await this.hashComparer.compare(authentication.password, account?.password);
+        }
         return null;
     }
 }
