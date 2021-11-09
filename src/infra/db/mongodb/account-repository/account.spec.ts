0580082cd8063288b865ee5f889cf5121af3d75e
castmoon <academicgmag@gmail.com>
2021-11-09 11:09:14 -0300

    test: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,5 +1,9 @@
+import { Collection } from 'mongodb';
+
 import { MongoHelper } from '../helpers/mongo-helper';
 import { AccountMongoRepository } from './account';
+
+let accountCollection: Collection;
 
 describe('Account Mongo Repository', () => {
     beforeAll(async () => {
@@ -11,7 +15,7 @@ describe('Account Mongo Repository', () => {
     });
 
     beforeEach(async () => {
-        const accountCollection = await MongoHelper.getCollection('accounts');
+        accountCollection = await MongoHelper.getCollection('accounts');
         await accountCollection.deleteMany({});
     });
 
@@ -19,13 +23,28 @@ describe('Account Mongo Repository', () => {
         return new AccountMongoRepository();
     };
 
-    test('should return an account on success', async () => {
+    test('should return an account on add success', async () => {
         const sut = makeSut();
         const account = await sut.add({
             name: 'any_name',
             email: 'any_email@email.com',
             password: 'any_password',
         });
+        expect(account).toBeTruthy();
+        expect(account.id).toBeTruthy();
+        expect(account.name).toBe('any_name');
+        expect(account.email).toBe('any_email@email.com');
+        expect(account.password).toBe('any_password');
+    });
+
+    test('should return an account on loadByEmail success', async () => {
+        const sut = makeSut();
+        await accountCollection.insertOne({
+            name: 'any_name',
+            email: 'any_email@email.com',
+            password: 'any_password',
+        });
+        const account = await sut.loadByEmail('any_email@email.com');
         expect(account).toBeTruthy();
         expect(account.id).toBeTruthy();
         expect(account.name).toBe('any_name');
