56bb08467ecc57431d0b5fb1bdc20ee6f1704348
castmoon <academicgmag@gmail.com>
2021-11-09 19:07:36 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import app from '../config/app';
 
-describe('SignUp routes', () => {
+describe('Login routes', () => {
     beforeAll(async () => {
         await MongoHelper.connect(process.env.MONGO_URL as string);
     });
@@ -16,15 +16,18 @@ describe('SignUp routes', () => {
         const accountCollection = await MongoHelper.getCollection('accounts');
         await accountCollection.deleteMany({});
     });
-    test('should return an account on success', async () => {
-        await request(app)
-            .post('/api/signup')
-            .send({
-                name: 'Guilherme',
-                email: 'guilherme@teste.com',
-                password: 'teste123',
-                passwordConfirmation: 'teste123',
-            })
-            .expect(200);
+
+    describe('POST /signup', () => {
+        test('should return 200 on signup', async () => {
+            await request(app)
+                .post('/api/signup')
+                .send({
+                    name: 'Guilherme',
+                    email: 'guilherme@teste.com',
+                    password: 'teste123',
+                    passwordConfirmation: 'teste123',
+                })
+                .expect(200);
+        });
     });
 });
