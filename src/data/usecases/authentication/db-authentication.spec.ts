e64bdc360e85138b6756883a43c2ed5b9bad6b11
castmoon <academicgmag@gmail.com>
2021-11-09 10:57:06 -0300

    refactor: rename update with updateAccessToken

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -52,7 +52,7 @@ const makeUpdateAccessTokenRepository = (): IUpdateAccessTokenRepository => {
     class MakeUpdateAccessTokenRepositoryStub
         implements IUpdateAccessTokenRepository
     {
-        async update(id: string, token: string): Promise<void> {
+        async updateAccessToken(id: string, token: string): Promise<void> {
             return new Promise((resolve) => resolve());
         }
     }
@@ -169,7 +169,10 @@ describe('DbAuthentication UseCase', () => {
 
     test('should call UpdateAccessTokenRepository with correct values', async () => {
         const { sut, updateAccessTokenRepositoryStub } = makeSut();
-        const updateSpy = jest.spyOn(updateAccessTokenRepositoryStub, 'update');
+        const updateSpy = jest.spyOn(
+            updateAccessTokenRepositoryStub,
+            'updateAccessToken'
+        );
         await sut.auth(makeFakeAuthentication());
         expect(updateSpy).toHaveBeenCalledWith('any_id', 'any_token');
     });
@@ -178,7 +181,7 @@ describe('DbAuthentication UseCase', () => {
         const { sut, updateAccessTokenRepositoryStub } = makeSut();
         jest.spyOn(
             updateAccessTokenRepositoryStub,
-            'update'
+            'updateAccessToken'
         ).mockReturnValueOnce(
             new Promise((resolve, reject) => reject(new Error()))
         );
