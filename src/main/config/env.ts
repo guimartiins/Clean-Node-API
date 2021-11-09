876aef7860633bca463f87cb7a129de9bb86846c
castmoon <academicgmag@gmail.com>
2021-11-09 19:15:42 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,4 +3,5 @@ export default {
         process.env.MONGO_URL ||
         'mongodb+srv://cleancode:rVklC2Xv31jvujJI@cluster0.2x4mx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || 'ts4932mfsdnl',
 };
---
+++ src/main/factories/login/login-factory.ts
@@ -0,0 +1,29 @@
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication';
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter';
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository';
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository';
+import { JwtAdapter } from '../../../infra/jwt-adapter/jwt-adapter';
+import { LoginController } from '../../../presentation/controllers/login/login-controller';
+import { IController } from '../../../presentation/protocols';
+import env from '../../config/env';
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator';
+import { makeLoginValidation } from './login-validation-factory';
+
+export const makeLoginController = (): IController => {
+    const salt = 12;
+    const bcryptAdapter = new BcryptAdapter(salt);
+    const jwtAdapter = new JwtAdapter(env.jwtSecret);
+    const accountMongoRepository = new AccountMongoRepository();
+    const dbAuthentication = new DbAuthentication(
+        accountMongoRepository,
+        bcryptAdapter,
+        jwtAdapter,
+        accountMongoRepository
+    );
+    const loginController = new LoginController(
+        dbAuthentication,
+        makeLoginValidation()
+    );
+    const logMongoRepository = new LogMongoRepository();
+    return new LogControllerDecorator(loginController, logMongoRepository);
+};
